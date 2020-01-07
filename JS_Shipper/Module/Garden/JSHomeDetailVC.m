0624ee53420b0125fce432f904ee2dcd99e6b29f
zyl <zengyulu@evcoming.com>
2020-01-07 10:22:03 +0800

    提现Ui修改 隐藏收藏功能 和聊天功能

--- JS_Shipper/Module/Garden/JSHomeDetailVC.m
+++ JS_Shipper/Module/Garden/JSHomeDetailVC.m
@@ -22,7 +22,7 @@
     [self.collectBtn setImage:[UIImage imageNamed:@"app_navigationbar_collection_default"] forState:UIControlStateNormal];
     [self.collectBtn setImage:[UIImage imageNamed:@"app_navigationbar_collection_selected"] forState:UIControlStateSelected];
     [self.collectBtn addTarget:self action:@selector(collectAction) forControlEvents:UIControlEventTouchUpInside];
-    self.navItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.collectBtn];
+//    self.navItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.collectBtn];
     
     [self createBottomView];
 }
@@ -31,7 +31,7 @@
     CGFloat viewH = 50;
     CGFloat bottomY = HEIGHT-viewH-kTabBarSafeH;
     CGFloat firstViewW = (WIDTH/2.0-30)/2.0;
-    self.callBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, bottomY, firstViewW, viewH)];
+    self.callBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, bottomY, firstViewW*2, viewH)];
     [self.callBtn setImage:[UIImage imageNamed:@"home_list_btn_phone"] forState:UIControlStateNormal];
     self.callBtn.backgroundColor = [UIColor whiteColor];
     [self.callBtn addTarget:self action:@selector(callAction) forControlEvents:UIControlEventTouchUpInside];
@@ -41,9 +41,9 @@
     self.chatBtn.backgroundColor = [UIColor whiteColor];
     [self.chatBtn setImage:[UIImage imageNamed:@"home_list_btn_chat"] forState:UIControlStateNormal];
     [self.chatBtn addTarget:self action:@selector(chatAction) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:self.chatBtn];
+//    [self.view addSubview:self.chatBtn];
     
-    self.cretaeOrderBtn = [[UIButton alloc]initWithFrame:CGRectMake(self.chatBtn.right, bottomY, WIDTH-self.chatBtn.right, viewH)];
+    self.cretaeOrderBtn = [[UIButton alloc]initWithFrame:CGRectMake(self.callBtn.right, bottomY, WIDTH-self.callBtn.right, viewH)];
     self.cretaeOrderBtn.backgroundColor = AppThemeColor;
     [self.cretaeOrderBtn addTarget:self action:@selector(createOrderAction) forControlEvents:UIControlEventTouchUpInside];
     [self.cretaeOrderBtn setTitle:@"立即下单" forState:UIControlStateNormal];
